b9a29b176775410e243bf2fe9182b754a3d1b081
GallagherStephen <37175825+GallagherStephen@users.noreply.github.com>
2021-02-20 14:17:26 +0000

    added new page for classes with content + css file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/home';
+import Classes from './components/classes';
 import Help from './components/help';
 import Webcam from './components/webcam';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from "react-bootstrap";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Classes from './components/classes';
 
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
         <h4 class="navbar-brand" href="/">Student-Mania</h4>
           <Nav>
             <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/classes">classes</Nav.Link>
             <Nav.Link href="/help">Help</Nav.Link>
             <Nav.Link href="/webcam">webcam</Nav.Link>
           </Nav>
@@ -25,6 +28,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/home' component={Home}/>
+          <Route exact path='/classes' component={Classes}/>
           <Route exact path='/help' component={Help}/>
           <Route exact path='/webcam' component={Webcam}/>
         </Switch>
