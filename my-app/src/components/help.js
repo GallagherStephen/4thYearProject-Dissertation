1248ae01b81171587965a577b6ee0ca01bac58ae
GallagherStephen <37175825+GallagherStephen@users.noreply.github.com>
2021-02-20 21:22:13 +0000

    took out video to see will it deploy

--- my-app/src/components/help.js
+++ my-app/src/components/help.js
@@ -24,7 +24,7 @@ class Help extends React.Component{
               </div>
 
               <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/vX-YnSQkRyk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              
               </div>
               
       </section>
