c4f674634ff3ec91caf4ebb71da9ea806d8f2b40
GallagherStephen <37175825+GallagherStephen@users.noreply.github.com>
2021-05-03 18:54:34 +0100

    removed emails and added them to linkdtree + fixing css

--- my-app/src/components/chat.js
+++ my-app/src/components/chat.js
@@ -3,11 +3,26 @@ import { ChatEngine } from 'react-chat-engine';
 
 function chat() {
 	return (
+
+
+		<body>
+			<main>
+			<section class="presentation">
+               
+			   
 		<ChatEngine
 			projectID='5fb755cb-318f-4a29-a038-ab46c69af8ea'
 			userName='admin'
 			userSecret='admin'
 		/>
+
+		  
+   			</section>
+			
+			</main>
+		
+		</body>
+		
 	);
 }
 
